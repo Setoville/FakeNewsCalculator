ce74101daa0be7b377b3c8e4c37f28f1aee7d555
Setoville <andrewseto@live.com>
2018-11-24 11:10:54 -0500

    sanity

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import http.client
 
 app = Flask(__name__)
 
-@app.route('/article',methods=['POST'])
+@app.route('/article',methods=['GET','POST'])
 def parse_article():
     if request.method == 'POST':
         return request.form['articleURL']
+    if request.method == 'GET':
+        return "hello world"
