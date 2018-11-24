fa974ef84d8a62a1fc18d59ed328c18c7f831136
Setoville <andrewseto@live.com>
2018-11-24 12:20:16 -0500

    add backup RNG

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 from flask_cors import CORS
+from random import randint
 import http.client
 
 app = Flask(__name__)
@@ -11,7 +13,13 @@ def parse_article():
         print (request.form.get("articleURL"))
         return "hello world"
     
-@app.route('/hello',methods=['GET'])
+@app.route('/test',methods=['GET'])
 def hello_world():
     print (request.headers['articleURL'])
     return "hello world"
+
+@app.route('/random',methods=['GET'])
+def random():
+    return jsonify(
+        score=randint(0,100)
+    )
