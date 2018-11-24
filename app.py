bd6654a5b2f8692ca4a9c664837f3f3e02baff5b
Setoville <andrewseto@live.com>
2018-11-24 12:33:13 -0500

    add url encoding

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS
 from random import randint
+import base64
 import http.client
 
 app = Flask(__name__)
 
-@app.route('/article',methods=['GET','POST'])
+
+@app.route('/article',methods=['GET'])
 def parse_article():
-    if request.method == 'POST':
-        print (request.form.get("articleURL"))
-        return "hello world"
     
-@app.route('/test',methods=['GET'])
-def hello_world():
-    print (request.headers['articleURL'])
-    return "hello world"
+    encodedArticleURLfromHeader = request.args.get('articleURL',default = None,type=str)
+
+    if articleURLfromHeader is not None:
+        #decode base64
+        decodedArticleURLfromHeader = base64.b64decode(encodedArticleURLfromHeader)
+
+        print (decodedArticleURLfromHeader)
+        return 0
+
+    
+    return 1
 
 @app.route('/random',methods=['GET'])
 def random():
