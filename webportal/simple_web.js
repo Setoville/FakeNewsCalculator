07dad0c98ef2c261a3dffc1ca39c862ae3fe87c0
joshualarbi <jkarbi3@uwo.ca>
2018-11-25 01:42:01 -0500

    Appending http:// if not entered

--- webportal/simple_web.js
+++ webportal/simple_web.js
@@ -63,9 +63,17 @@ window.addEventListener("keyup", function(event) {
   }
 });
 
+function append_http(text) {
+    if (!text.startsWith("http://") && !text.startsWith("https://")) {
+        text = "http://"+text;
+    }
+    return text;
+}
+
 function calculate_fake_news_score() {
 
 	entered_url = document.getElementById("url").value;
+    entered_url = append_http(entered_url);
 	console.log("ENTERED URL: " + entered_url);
 
 	var encoded_url =  base64Encode(entered_url);
